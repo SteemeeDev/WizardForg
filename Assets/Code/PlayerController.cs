6d875acd7a6c533fe54b740742a796d5741ec170
Steamy <toretordenguden@gmail.com>
2026-03-05 13:23:29 +0100

    a

--- Assets/Code/PlayerController.cs
+++ Assets/Code/PlayerController.cs
@@ -4,9 +4,17 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    [SerializeField] CharacterController controller;
+    [SerializeField] float moveSpeed = 4f;
+
     private void Update()
     {
         float inputX = Input.GetAxisRaw("Horizontal");
         float inputY = Input.GetAxisRaw("Vertical");
+
+        Vector3 moveDir = (Quaternion.Euler(0, 45, 0) * new Vector3(inputX, 0, inputY));
+        moveDir = Vector3.Normalize(moveDir);
+
+        controller.Move(moveDir * Time.deltaTime * moveSpeed);
     }
 }
---
+++ Assets/Code/WandController.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WandController : MonoBehaviour
+{
+    [SerializeField] Transform playerTransform;
+
+    [SerializeField] float rot = 0;
+
+    Camera cam;
+
+    private void Start()
+    {
+        cam = Camera.main;
+    }
+
+    private void Update()
+    {
+        Vector3 playerScreenPos = cam.WorldToScreenPoint(playerTransform.position);
+        Vector3 mousePos = Input.mousePosition;
+
+        Vector3 playerLook = playerScreenPos - mousePos;
+
+        playerLook = playerLook.normalized;
+
+        transform.position = playerTransform.position - Quaternion.Euler(0, 45, 0) * new Vector3(playerLook.x, 0, playerLook.y);
+
+        transform.rotation = Quaternion.AngleAxis(rot, transform.up);
+    }
+}
