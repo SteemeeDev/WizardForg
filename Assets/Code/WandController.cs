27b47139bbcd256d7b0c424a8715e4f032015958
Steamy <toretordenguden@gmail.com>
2026-03-05 13:52:08 +0100

    a

--- Assets/Code/WandController.cs
+++ Assets/Code/WandController.cs
@@ -5,8 +5,9 @@ using UnityEngine;
 public class WandController : MonoBehaviour
 {
     [SerializeField] Transform playerTransform;
-
     [SerializeField] float rot = 0;
+
+    [SerializeField] Vector3 forward = new Vector3(-45, -135, 0);
 
     Camera cam;
 
@@ -26,6 +27,6 @@ public class WandController : MonoBehaviour
 
         transform.position = playerTransform.position - Quaternion.Euler(0, 45, 0) * new Vector3(playerLook.x, 0, playerLook.y);
 
-        transform.rotation = Quaternion.AngleAxis(rot, transform.up);
+        //transform.rotation = Quaternion.LookRotation(forward) * Quaternion.AngleAxis(rot, forward);
     }
 }
