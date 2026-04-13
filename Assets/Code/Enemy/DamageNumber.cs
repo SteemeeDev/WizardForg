a23120f99e22b840799ca7af1f1c04aef3161d50
Steamy <toretordenguden@gmail.com>
2026-04-13 13:05:47 +0200

    Progress uopdate

---
+++ Assets/Code/Enemy/DamageNumber.cs
@@ -0,0 +1,22 @@
+using TMPro;
+using UnityEngine;
+
+public class DamageNumber : MonoBehaviour
+{
+    public TMP_Text text;
+    [SerializeField] float lifeTime = 3f;
+    Rigidbody rb;
+
+    float fallSpeed = 3f;
+    private void Start()
+    {
+        text = GetComponent<TMP_Text>();
+        rb = GetComponent<Rigidbody>();
+        rb.velocity = new Vector3(Random.Range(-3f, 3f), 2, Random.Range(-3f, 3f));
+    }
+
+    private void Update()
+    {
+        rb.velocity -= Vector3.up * Time.deltaTime * Time.deltaTime * fallSpeed;
+    }
+}
